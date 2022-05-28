ed7df18b4cae71e086673ba4ab89c17cb5c8b271
Abel López Parrado <abellp09.3@gmail.com>
2022-05-28 20:43:32 +0200

    Fix __init__.py módulo tablas

--- api/bbdd/tablas/__init__.py
+++ api/bbdd/tablas/__init__.py
@@ -2,5 +2,6 @@ from .tabla_asignatura import Asignatura
 from .tabla_aula import Aula
 from .tabla_dia_semana import DiaSemana
 from .tabla_grupo import Grupo
+from .tabla_horario import Horario
 from .tabla_profesor import Profesor
 from .tabla_tramo_horario import TramoHorario
