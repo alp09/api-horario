c8224ce973ec3900256bc9b8f34a738772f86206
Abel López Parrado <abellp09.3@gmail.com>
2022-05-29 19:55:24 +0200

    Modificado bbdd.py para generar tablas si no existen

--- api/config/__init__.py
+++ api/config/__init__.py
@@ -2,6 +2,10 @@ from .path_settings import (
 	BASE_URL,
 	SECRET_KEY,
 	ALGORITMO,
-	path_archivo_google,
-	path_archivo_settings
+	DB_USERNAME,
+	DB_PASSWORD,
+	DB_HOST,
+	DB_PORT,
+	DB_DATABASE,
+	path_archivo_google
 )
