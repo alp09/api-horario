f8fb4fee40f3744b2b26e314cdb602190fea094b
Abel López Parrado <abellp09.3@gmail.com>
2022-06-03 23:08:42 +0200

    Abreviado nombre columna

--- api/bbdd/tablas/tabla_profesor.py
+++ api/bbdd/tablas/tabla_profesor.py
@@ -7,7 +7,7 @@ class Profesor(Base):
 	__tablename__ = "profesor"
 
 	# Columnas
-	codigo 			= Column(String(20), primary_key=True)
-	nombre_completo = Column(String(255))
-	email			= Column(String(255), unique=True)
-	es_admin		= Column(Boolean, default=False)
+	codigo		= Column(String(20), primary_key=True)
+	nombre		= Column(String(255))
+	email		= Column(String(255), unique=True)
+	es_admin	= Column(Boolean, default=False)
--- api/esquemas/esquemas_profesor.py
+++ api/esquemas/esquemas_profesor.py
@@ -2,10 +2,10 @@ from pydantic import BaseModel, Field
 
 
 class Profesor(BaseModel):
-	codigo: str 		 = Field(max_length=20)
-	nombre_completo: str = Field(max_length=255)
-	email: str | None	 = Field(None, max_length=255)
-	es_admin: bool		 = False
+	codigo: str 		= Field(max_length=20)
+	nombre: str 		= Field(max_length=255)
+	email: str | None 	= Field(None, max_length=255)
+	es_admin: bool 		= Field(False)
 
 	class Config:
 		orm_mode = True
