1276c40b4b8b2649a8c58d86e569ff5260f85d20
Abel López Parrado <abellp09.3@gmail.com>
2022-06-10 18:48:22 +0200

    Simplificado modelos horario y reserva

--- src/institutoapi/bbdd/modelos/modelo_reserva.py
+++ src/institutoapi/bbdd/modelos/modelo_reserva.py
@@ -1,44 +1,53 @@
 from datetime import date
+
+from pydantic import validator
 from sqlmodel import SQLModel, Field, Relationship
 
 from institutoapi.bbdd.modelos import TramoHorario, Asignatura, Profesor, Aula, Grupo
 
 
-class Reserva(SQLModel, table=True):
+class ReservaRequest(SQLModel):
+
+	# Columnas
+	fecha: date				= Field()
+	id_tramo: int			= Field(foreign_key="tramo_horario.id")
+	codigo_asignatura: str	= Field(foreign_key="asignatura.codigo", max_length=20)
+	codigo_profesor: str	= Field(foreign_key="profesor.codigo", max_length=20)
+	codigo_aula: str		= Field(nullable=True, default=None, foreign_key="aula.codigo", max_length=20)
+	codigo_grupo: str		= Field(nullable=True, default=None, foreign_key="grupo.codigo", max_length=20)
+
+	@validator("fecha")
+	def validar_fecha_no_esta_en_pasado(cls, fecha):
+		assert not fecha < date.today(), "La fecha de la reserva no puede estar en el pasado"
+		return fecha
+
+	@validator("fecha")
+	def validar_fecha_no_este_fin_de_semana(cls, fecha):
+		# isoweekday() devuelve el día de la semana, siendo el lunes = 1 y el domingo = 7
+		assert fecha.isoweekday() < 6, "No se puede hacer una reserva los fines de semana"
+		return fecha
+
+
+class Reserva(ReservaRequest, table=True):
 	__tablename__ = "reserva"
 
 	# Columnas
 	id: int					= Field(primary_key=True)
-	fecha: date				= Field()
-	id_tramo: int			= Field(foreign_key="tramo_horario.id")
-	codigo_asignatura: str	= Field(foreign_key="asignatura.codigo")
-	codigo_profesor: str	= Field(foreign_key="profesor.codigo")
-	codigo_aula: str		= Field(nullable=True, default=None, foreign_key="aula.codigo")
-	codigo_grupo: str		= Field(nullable=True, default=None, foreign_key="grupo.codigo")
 
 	# Relaciones
-	tramo: TramoHorario 	= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
-	asignatura: Asignatura 	= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
-	profesor: Profesor 		= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
-	aula: Aula 				= Relationship(sa_relationship_kwargs={"lazy": "joined"})
-	grupo: Grupo 			= Relationship(sa_relationship_kwargs={"lazy": "joined"})
-
-
-class ReservaRequest(SQLModel):
-
-	# Campos
-	fecha: date
-	id_tramo: int
-	codigo_asignatura: str	= Field(max_length=20)
-	codigo_profesor: str	= Field(max_length=20)
-	codigo_aula: str		= Field(max_length=20)
-	codigo_grupo: str		= Field(max_length=20)
+	tramo: TramoHorario		= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
+	asignatura: Asignatura	= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
+	profesor: Profesor		= Relationship(sa_relationship_kwargs={"lazy": "joined", "innerjoin": True})
+	aula: Aula				= Relationship(sa_relationship_kwargs={"lazy": "joined"})
+	grupo: Grupo			= Relationship(sa_relationship_kwargs={"lazy": "joined"})
 
 
 class ReservaResponse(SQLModel):
 
-	# Campos
+	# Columnas
 	id: int
+
+	# Relaciones
 	tramo: TramoHorario
 	asignatura: Asignatura
 	profesor: Profesor
